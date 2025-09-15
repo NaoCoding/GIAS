7d222d2ca9b25d7671d11eaf48324be3ff1ae173
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2025-09-15 21:37:13 +0800

    fix(lint): remove unused package and add return type for `get_issue_by_issue_id` function

--- test/check_github.py
+++ test/check_github.py
@@ -3,6 +3,5 @@ sys.path.append(sys.path[0].split("test")[0])
 
 from tools import github_tool as gt
 
-print(
-    gt.get_issue_by_issue_id("plait-board/drawnix" , 297)
-)
+issue = gt.get_issue_by_issue_id("plait-board/drawnix" , 297)
+print(issue.title)
--- tools/github_tool.py
+++ tools/github_tool.py
@@ -1,16 +1,13 @@
-import requests
 import os
 from dotenv import load_dotenv
 
-from github import Github
-from github import Auth
+import github
 
 load_dotenv()
 
-def get_issue_by_issue_id(repo: str , id: int):
+def get_issue_by_issue_id(repo: str , id: int) -> github.Issue.Issue:
 
-    # github_auth = Auth.Token(os.getenv("GITHUB_TOKEN"))
-    g = Github(os.getenv("GITHUB_TOKEN"))
+    g = github.Github(os.getenv("GITHUB_TOKEN"))
     result = g.get_repo(repo).get_issue(id)
     g.close()
     return result
